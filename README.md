8c583e9883e1a294a051c8bfc3ab45f0985a5583
Aulia Mahardika <aulia.mahardika@live.com>
2022-12-26 13:30:00 +0700

    update calendar label

---
+++ README.md
@@ -0,0 +1,9 @@
+# spm-calendar
+
+A description of this package.
+
+Simple calendar label widget Swift UI
+
+A custom build swift package for calendar ui
+
+build for modular practice
---
+++ Sources/spm-calendar/spm_calendar.swift
@@ -0,0 +1,13 @@
+@main
+public struct spm_calendar {
+    
+      
+
+    public private(set) var text = "Hello, World!"
+
+
+    public static func main() {
+        print(spm_calendar().text)
+    
+    }
+}
